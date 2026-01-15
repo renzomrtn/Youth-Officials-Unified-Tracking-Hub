870a98fa2ac57d302ad97092071bc94565567aa5
renzomrtn <martinlorenz.fajardo@gmail.com>
2026-01-15 14:33:23 +0800

    proper table

--- frontend/src/views/Project/ProjectsView.vue
+++ frontend/src/views/Project/ProjectsView.vue
@@ -4,60 +4,61 @@
             <TabsList v-if="hasTabs" />
         </div>
         <div class="inner-content">
-            <v-table striped="even" hover rounded class="tables rounded-lg overflow-hidden">
-                <thead>
-                    <tr class="table-header">
-                        <td>
-                        </td>
-                        <td>
-                            Projects for 2025
-                        </td>
-                    </tr>
-                </thead>
-                <thead>
-                    <tr class="row-header">
-                        <th class="text-left">
-                        </th>
-                        <th class="text-left">
-                            Proponent
-                        </th>
-                        <th class="text-left">
-                            Project Title
-                        </th>
-                        <th class="text-left">
-                            Line Item Information
-                        </th>
-                        <th class="text-left">
-                            Budgeting Information
-                        </th>
-                        <th class="text-left">
-                            Date Information
-                        </th>
-                        <th class="text-left">
-                            Progress & Status
-                        </th>
-                        <th class="text-left">
-                            Expense Verification
-                        </th>
-                        <th class="text-left">
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in desserts" :key="item.name">
-                        <td>V</td>
-                        <td>{{ item.name }}</td>
-                        <td>{{ item.calories }}</td>
-                        <td>{{ item.name }}</td>
-                        <td>{{ item.calories }}</td>
-                        <td>{{ item.name }}</td>
-                        <td>{{ item.calories }}</td>
-                        <td>{{ item.name }}</td>
-                        <td>{{ item.calories }}</td>
-                    </tr>
-                </tbody>
-            </v-table>
+            <section class="holder">
+                <div class="wrapper">
+                    <h1>Projects for 2025</h1>
+                    <div class="toolbar">
+                        <v-text-field label="Label"></v-text-field>
+                        <v-btn>+ Add Project</v-btn>
+                    </div>
+                </div>
+                <v-table striped="even" hover rounded class="tables rounded-lg overflow-hidden">
+                    <thead>
+                        <tr class="row-header">
+                            <th class="text-left">
+                            </th>
+                            <th class="text-left">
+                                Proponent
+                            </th>
+                            <th class="text-left">
+                                Project Title
+                            </th>
+                            <th class="text-left">
+                                Line Item Information
+                            </th>
+                            <th class="text-left">
+                                Budgeting Information
+                            </th>
+                            <th class="text-left">
+                                Date Information
+                            </th>
+                            <th class="text-left">
+                                Progress & Status
+                            </th>
+                            <th class="text-left">
+                                Expense Verification
+                            </th>
+                            <th class="text-left">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in desserts" :key="item.name">
+                            <td>V</td>
+                            <td>{{ item.name }}</td>
+                            <td>{{ item.calories }}</td>
+                            <td>{{ item.name }}</td>
+                            <td>{{ item.calories }}</td>
+                            <td>{{ item.name }}</td>
+                            <td>{{ item.calories }}</td>
+                            <td>{{ item.name }}</td>
+                            <td>{{ item.calories }}</td>
+                        </tr>
+                    </tbody>
+                </v-table>
+                <v-pagination :length="4"></v-pagination>
+            </section>
         </div>
     </section>
 </template>
@@ -69,25 +70,25 @@ import TabsList from '@/components/TabsList.vue'
 const { hasTabs } = useTabs()
 
 const desserts = ref([
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-  },
+    {
+        name: 'Frozen Yogurt',
+        calories: 159,
+    },
+    {
+        name: 'Ice cream sandwich',
+        calories: 237,
+    },
+    {
+        name: 'Eclair',
+        calories: 262,
+    },
+    {
+        name: 'Cupcake',
+        calories: 305,
+    },
+    {
+        name: 'Gingerbread',
+        calories: 356,
+    },
 ])
 </script>
